eb1630e9c6c637975abfe94fb1b04e3f3420d572
Robert Utterback <robertu13@gmail.com>
2016-03-07 09:18:58 -0600

    Had to change signature of __cilkrts_resume_suspended to indicate whether or not the to-be-suspended deque should be resumable by another worker.

--- cilkrtssuspend/include/internal/abi.h
+++ cilkrtssuspend/include/internal/abi.h
@@ -482,7 +482,7 @@ CILK_ABI(void) __cilkrts_suspend(void);
 
 CILK_ABI(void*) __cilkrts_get_deque(void);
 CILK_ABI(void) __cilkrts_suspend_deque(void);
-CILK_ABI(void) __cilkrts_resume_suspended(void*);
+CILK_ABI(void) __cilkrts_resume_suspended(void*, int);
 
 /**
  * Resumes the runtime by notifying the workers that they can steal.

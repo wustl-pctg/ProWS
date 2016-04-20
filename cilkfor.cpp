067b37e5f57509ef93cb1454ef526229f7b46b40
Robert Utterback <robertu13@gmail.com>
2016-04-20 08:49:32 -0500

    minor changes to cilkfor

--- cilkfor.cpp
+++ cilkfor.cpp
@@ -9,18 +9,21 @@ cilkrr::mutex g_mutex;
 int main(int argc, char *argv[])
 {
 	int n = (argc > 1) ? atoi(argv[1]) : 5;
-	fprintf(stderr, "Begin: %s\n", cilkrr::get_pedigree().c_str());
+	int count = 0;
+	//fprintf(stderr, "Begin: %s\n", cilkrr::get_pedigree().c_str());
+
+#pragma cilk grainsize=1
 	cilk_for(int i = 0; i < n; ++i) {
 
-		__cilkrts_bump_loop_rank();
+		//__cilkrts_bump_loop_rank();
 
-		fprintf(stderr, "i(%i): %s\n", i,
-						cilkrr::get_pedigree().c_str());
+		// fprintf(stderr, "i(%i): %s\n", i,
+		// 				cilkrr::get_pedigree().c_str());
 
-		// g_mutex.lock();
-		// g_mutex.unlock();
-
+		g_mutex.lock();
+		count++;
+		g_mutex.unlock();
 	}
-	fprintf(stderr, "End: %s\n", cilkrr::get_pedigree().c_str());
+	//fprintf(stderr, "End: %s\n", cilkrr::get_pedigree().c_str());
 	return 0;
 }

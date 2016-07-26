83e3b1acb75b5c192b2e833aded922d8c3312e06
Robert Utterback <robertu13@gmail.com>
2016-07-26 12:09:52 -0500

    makefile changes

--- bench/bench.mk
+++ bench/bench.mk
@@ -1,10 +1,10 @@
 # This file defins BASIC_CFLAGS, BASIC_CXXFLAGS, LDFLAGS, LDLIBS, TOOL_FLAGS, TOOL_LDFLAGS
 # Makefile including this file should use these flags 
 
-COMPILER_DIR = $(HOME)/sandbox/llvm-cilk
-CILKRTS_INSTALL = $(HOME)/sandbox/cilkrecord/cilkrtssuspend/build
-CILKRR_INSTALL = $(HOME)/sandbox/cilkrecord/
-BENCH_DIR = $(HOME)/sandbox/cilkrecord/bench
+COMPILER_DIR = $(HOME)/src/llvm-cilk
+CILKRTS_INSTALL = $(HOME)/src/llvm-cilk
+CILKRR_INSTALL = $(HOME)/devel/cilkrecord/
+BENCH_DIR = $(HOME)/devel/cilkrecord/bench
 
 CC = $(COMPILER_DIR)/bin/clang 
 CXX = $(COMPILER_DIR)/bin/clang++
@@ -17,8 +17,9 @@ else
 endif
 
 INC += -I$(COMPILER_DIR)/include -I$(CILKRTS_INSTALL)/include
-INC += -I$(BENCH_DIR)
+INC += -I$(BENCH_DIR) -I$(CILKRR_INSTALL)
 LIBS += -ldl -lpthread $(CILKRR_LIBS) $(CILKRTS_INSTALL)/lib/libcilkrts.a 
 # LDFLAGS += -L$(CILKRTS_INSTALL)/lib
-BASIC_CFLAGS += -O3 -g -fcilkplus $(CILKRR_CFLAGS) $(INC) # -Wfatal-errors
-BASIC_CXXFLAGS = $(BASIC_CFLAGS)
+BASIC_FLAGS = -O0 -g -fcilkplus $(CILKRR_FLAGS) $(INC) #-Wfatal-errors
+BASIC_CFLAGS += $(BASIC_FLAGS) -std=gnu11
+BASIC_CXXFLAGS = $(BASIC_FLAGS) -std=gnu++11

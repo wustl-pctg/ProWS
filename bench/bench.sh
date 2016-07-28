c98de5edf1e3fd7a70946d2d9260cd2e8062f8e0
robertutterback <robertu13@gmail.com>
2016-07-27 20:35:46 -0500

    bench script improvements

--- bench/bench.sh
+++ bench/bench.sh
@@ -4,17 +4,19 @@ ulimit -v 6291456
 # Don't forget that we might need to set vm.max_map_limit
 trap "kill -- -$$" SIGINT
 
-MAXTIME="2m"
+MAXTIME="5m"
 CORES="1 2 4 6 8"
-NITER=3
+BASE_ITER=3
+RECORD_ITER=5
+REPLAY_ITER=10
 
-bench=(ferret) #dedup ferret kdtree
+bench=(dedup ferret) #dedup ferret kdtree
 declare -A dirs args
 
 dirs["dedup"]=dedup
-args["dedup"]="small" # native
+args["dedup"]="native"
 dirs["ferret"]=ferret
-args["ferret"]="small"
+args["ferret"]="large"
 
 errcheck () {
     errcode=$1
@@ -24,20 +26,20 @@ errcheck () {
         printf "%0.2f" "$(grep 'Processing time' log | tail -1 | cut -d' ' -f 4 | tr -d ' ')"
     else
         printf "\n[Error]:\n"
-				if [[ $errcode -eq 124 ]]; then
-						printf "Timed out after %s.\n" $MAXTIME
-						exit 1
-				fi
+        if [[ $errcode -eq 124 ]]; then
+            printf "Timed out after %s.\n" $MAXTIME
+            exit 1
+        fi
         cat log
         exit 1
     fi
 }
 
 runcmd() {
-		P=$1
-		mode=$2
-		name=$3
-		args=$4
+    P=$1
+    mode=$2
+    name=$3
+    args=$4
     str="CILKRR_MODE=$mode ./$name lock $args out $P &> log"
     CILKRR_MODE=$mode ./$name lock $args out $P &> log
 }
@@ -45,21 +47,21 @@ runcmd() {
 runreplay() {
     name=$1
     args=$2
-    REPLAYITER=$NITER
-
+    declare -A vals
+    
     for P in $CORES; do
-				avg=0
-				for i in $(seq $REPLAYITER); do
-						runcmd "$P" "replay" "$name" "$args"
-						val=$(errcheck $? "log")
-						if [[ $? -ne 0 ]]; then
-								printf "Error\n"
-								exit 1
-						fi
-						avg=$( echo "scale=2; $avg + $val" | bc )
-				done
-				avg=$( echo "scale=2; $avg / $REPLAYITER" | bc )
-				printf "\t%s" "$avg"
+        for i in $(seq $REPLAY_ITER); do
+            runcmd "$P" "replay" "$name" "$args"
+            val=$(errcheck $? "log")
+            if [[ $? -ne 0 ]]; then
+                printf "Error\n"
+                exit 1
+            fi
+            vals[$i]=$val
+        done
+        avg=$( echo ${vals[@]} | tr " " "\n" | datamash mean 1 | tr -s " ")
+        stdev=$( echo ${vals[@]} | tr " " "\n" | datamash sstdev 1 | tr -s " ")
+        printf "\t%.2f(%.2f)" "$avg" "$stdev"
     done
 
 }
@@ -70,45 +72,52 @@ runall () {
     name="run.sh"
     args=$2
     
-    printf -- "--- $name ---\n"
-    header_left="P\tbase\trecord"
-		printf "${header_left}\t\t\treplayP\n"
-		printf "%${#header_left}s\t" | tr " " "=" | tr "\t" "====="
-		for P in $CORES; do printf "\t$P"; done;
-		printf "\n"
+    printf -- "--- $name $args ---\n"
+    header_left="P\tbase\t\trecord"
+    printf "${header_left}\t\t\treplayP\n"
+    printf "%${#header_left}s\t\t\t\t\t\t\t\t\t" | tr " " "=" | tr "\t" "====="
+    for P in $CORES; do printf "\t\t$P"; done;
+    printf "\n"
+
+    declare -A vals
 
     for P in $CORES; do
-				printf "$P"
-				avg=0
-				for i in $(seq $NITER); do
-						runcmd "$P" "none" "run.sh" "$args"
-						val=$(errcheck $? "log")
-						if [[ $? -ne 0 ]]; then
-								printf "Error\n"
-								exit 1
-						fi
-						avg=$( echo "scale=2; $avg + $val" | bc )
-				done
-				avg=$( echo "scale=2; $avg / $NITER" | bc )
-				printf "\t%s" "$avg"
+        printf "$P"
+        for i in $(seq $BASE_ITER); do
+            runcmd "$P" "none" "run.sh" "$args"
+            val=$(errcheck $? "log")
+            if [[ $? -ne 0 ]]; then
+                printf "Error\n"
+                exit 1
+            fi
+            vals[$i]=$val
+        done
+        avg=$( echo ${vals[@]} | tr " " "\n" | datamash mean 1 | tr -s " ")
+        stdev=$( echo ${vals[@]} | tr " " "\n" | datamash sstdev 1 | tr -s " ")
+        printf "\t%.2f(%.2f)" "$avg" "$stdev"
 
-				avg=0
-				for i in $(seq $NITER); do
-						runcmd "$P" "record" "$name" "$args"
-						val=$(errcheck $? "log")
-						if [[ $? -ne 0 ]]; then
-								printf "Error\n"
-								exit 1
-						fi
 
-						avg=$( echo "scale=2; $avg + $val" | bc )
-				done
-				avg=$( echo "scale=2; $avg / $NITER" | bc )
-				printf "\t%s" "$avg"
+        rm -f .recordtimes
+        for i in $(seq $RECORD_ITER); do
+            runcmd "$P" "record" "$name" "$args"
+            val=$(errcheck $? "log")
+            if [[ $? -ne 0 ]]; then
+                printf "Error\n"
+                exit 1
+            fi
+            vals[$i]=$val
+            printf "%.2f\t%d\n" "$val" "$i" >> .recordtimes
+            mv .cilkrecord .cilkrecord.$i
+        done
+        avg=$( echo ${vals[@]} | tr " " "\n" | datamash mean 1 | tr -s " ")
+        stdev=$( echo ${vals[@]} | tr " " "\n" | datamash sstdev 1 | tr -s " ")
+        printf "\t%.2f(%.2f)" "$avg" "$stdev"
 
-				runreplay "$name" "$args"
-				printf "\n"
-		done
+        median=$(cat .recordtimes | sort | cut -f 2 | datamash median 1)
+        mv .cilkrecord.$median .cilkrecord
+        runreplay "$name" "$args"
+        printf "\n"
+    done
     printf "\n-------------------------"
     set -e
 

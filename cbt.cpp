8134a3394b6368c85bf576510f14f8b074acb3d1
Robert Utterback <robertu13@gmail.com>
2016-07-31 16:15:31 -0500

    Better binary spawn becnhmark

--- cbt.cpp
+++ cbt.cpp
@@ -12,6 +12,8 @@
 
 #include "mutex.h"
 
+// 2 ^ 23
+#define NUM_LOCK_ACQUIRES (1 << 23)
 int g_count = 0;
 double g_leaf_work = 0.0;
 cilkrr::mutex g_mutex;
@@ -34,12 +36,15 @@ void tree_search(int index, int level)
 
 int main(int argc, char *argv[])
 {
-	int depth = (argc > 1) ? std::atoi(argv[1]) : 10;
+	int depth = (argc > 1) ? std::atoi(argv[1]) : 5;
+  int iter = (argc > 2) ? std::atoi(argv[2]) : (NUM_LOCK_ACQUIRES >> depth);
 
   auto start = std::chrono::high_resolution_clock::now();
-  tree_search(0, depth);
+  for (int i = 0; i < iter; ++i)
+    tree_search(0, depth);
   auto end = std::chrono::high_resolution_clock::now();
-  assert(g_count == (1 << depth));
+  int expected = iter * (1 << depth);
+  assert(g_count == expected);
   std::cout << "time: "
             << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
             << std::endl;

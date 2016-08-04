0fc1679a9493e4b5c0f751c41e041e42a4332f01
Robert Utterback <robertu13@gmail.com>
2016-08-04 14:52:19 -0500

    better cilkfor benchmark

--- bench/micro/cilkfor.cpp
+++ bench/micro/cilkfor.cpp
@@ -1,67 +1,43 @@
 #include <iostream>
-#include <cstdio>
+#include <chrono> // timing
+
 #include <cassert>
-#include <chrono>
-#include <unistd.h>
+#include <unistd.h> // sleep()
+
 #include <cilk/cilk.h>
 #include <cilk/cilk_api.h>
 
 #include "mutex.h"
 #include "acquire.h"
 
-#define NLOOPS 10
-cilkrr::mutex g_mutex;
-
+// cilkfor <# acquires> <# locks> [lock work time (ms)] [unlock work time (ms)]
 int main(int argc, char *argv[])
 {
-  int niter = 1000;
-  int nacquires = 1;
-  int sleep_ms = 0;
-  int work_ms = 0;
+  size_t num_acquires = (argc > 1) ? std::strtoul(argv[1], nullptr, 0) : 10000;
+  size_t num_locks = (argc > 2) ? std::strtoul(argv[2], nullptr, 0) : 1;
+  double lock_time = (argc > 3) ? std::strtod(argv[3], nullptr)/1000.0 : 0.0;
+  double unlock_time = (argc > 4) ? std::strtod(argv[4], nullptr)/1000.0 : 0.0;
 
-  switch (argc) {
-  case 5:
-    work_ms = atoi(argv[4]);
-  case 4:
-    sleep_ms = atoi(argv[3]);
-  case 3:
-    nacquires = atoi(argv[2]);
-  case 2:
-    niter = atoi(argv[1]);
-  case 1:
-    break;
-  default:
-    fprintf(stderr, "Invalid # args\n");
-    std::abort();
-  }
-  
-	int count = 0;
-  double t1 = (double)sleep_ms / 1000.0;
-  double t2 = (double)work_ms / 1000.0;
+  assert(num_acquires % num_locks == 0);
+  size_t count = 0;
+  cilkrr::mutex *locks = new cilkrr::mutex[num_locks];
 
 	auto start = std::chrono::high_resolution_clock::now();
-  
-	for (int i = 0; i < NLOOPS; ++i) {
-    
 #pragma cilk grainsize = 1
-    cilk_for(int j = 0; j < niter; ++j) {
+	cilk_for (int i = 0; i < (num_acquires / num_locks); ++i) {
 
-      if (t1 > 0.0) sleep(t1);
+    if (unlock_time > 0.0) sleep(unlock_time);
     
-      for (int k = 0; k < nacquires; ++k) {
-        g_mutex.lock();
-
-        if (t2 > 0.0) sleep(t2);
-      
-        if (i % 2 == 0) count++;
-        else count--;
-      
-        g_mutex.unlock();
-      }
-    }
+    locks[i % num_locks].lock();
+    count++;
+    if (lock_time > 0.0) sleep(lock_time);
+    locks[i % num_locks].unlock();
   }
+
 	auto end = std::chrono::high_resolution_clock::now();
-  assert(count == 0);
+  assert(count == num_acquires);
   std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << std::endl;
+  
+  delete[] locks;
 	return 0;
   }

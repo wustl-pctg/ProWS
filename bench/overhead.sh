2b53585f59384e024e10baa99441a35c725edd5f
Robert Utterback <robertu13@gmail.com>
2016-08-10 13:12:37 -0500

    improved overhead script

--- bench/overhead.sh
+++ bench/overhead.sh
@@ -9,12 +9,12 @@ logfile=$BENCH_LOG_FILE
 echo "********** Begin script: $(realpath $0) **********" >> $logfile
 
 MAXTIME="5m"
-ITER=3
-CORES="1 2 4 8 12 16"
+ITER=5
+CORES="1"
 
 #refine MIS matching BFS chess dedup ferret
-bench=(MIS matching refine)
-basedir=..
+bench=(cbt cilkfor fib)
+basedir=../src
 source config.sh
 
 errcheck () {
@@ -24,7 +24,6 @@ errcheck () {
     #cat $logname
     if [[ $errcode -eq 0 ]]; then
         printf "%0.2f" "$(grep 'time' $logname | tail -1 | cut -d':' -f 2 | tr -d ' ')"
-        #printf "%0.2f" "$(grep 'time' $logname | tail -1 | cut -d' ' -f 4 | tr -d ' ')"
     else
         printf "\n[Error]:\n"
         if [[ $errcode -eq 124 ]]; then
@@ -48,7 +47,6 @@ runcmd() {
     fi
     cmd="CILK_NWORKERS=$P CILKRR_MODE=$mode ./$name $args"
     echo "$cmd" >> $logfile
-    #echo "$cmd"
     
     CILK_NWORKERS=$P CILKRR_MODE=$mode ./$name $args 2>&1 | tee -a $logfile &> .log
     echo "------------------------------------------------------------" >> $logfile
@@ -86,8 +84,8 @@ runmode () {
         #echo $val
     done
     avg=$( echo ${vals[@]} | tr " " "\n" | datamash mean 1 | tr -s " ")
-    # stdev=$( echo ${vals[@]} | tr " " "\n" | datamash sstdev 1 | tr -s " ")
-    printf "%10.2f" "$avg"
+    stdev=$( echo ${vals[@]} | tr " " "\n" | datamash sstdev 1 | tr -s " ")
+    printf "%10.2f(%5.2f)" "$avg" "$stdev"
     # printf "(%.2f)" "$stdev"
 
     make clean &> /dev/null
@@ -96,10 +94,11 @@ runmode () {
 
 newstage () {
     stage=$1
+    ptype=$2
     cd $basedir
     make clean &> /dev/null
 
-    make STAGE=$stage -j &> compile.log
+    make STAGE=$stage PTYPE=$ptype -j &> compile.log
     if [[ "$?" -ne 0 ]]; then
         echo "Compile error!"
         cat compile.log
@@ -111,20 +110,48 @@ newstage () {
 
 if [ $# -gt 0 ]; then bench=($@); fi
 
-printf "%-10s%5s%10s%10s%10s%10s\n" "bench" "P" "none" "get" "insert" "conflict"
+printf "%-10s%5s%10s%15s%15s%15s%15s\n" "bench" "P" "ped" "none" "get" "insert" "conflicts"
+#printf "%-10s%5s%15s%15s%15s%15s\n" "bench" "P" "pre-none" "pre-record" "array-none" "array-record"
 
 for b in "${bench[@]}"; do
-    printf "%-10s" "$b"
+    
     for P in $CORES; do
+        printf "%-10s" "$b"
         printf "%5s" $P
-        newstage "0"
+        printf "%10s" "array"
+        newstage "0" "2"
         runmode "$P" "$b" "${cmdnames[$b]}" "none" "${args[$b]}" # baseline
-        newstage "1"
+        newstage "1" "2"
         runmode "$P" "$b" "${cmdnames[$b]}" "record" "${args[$b]}" # get pedigree
-        newstage "2"
+        newstage "2" "2"
         runmode "$P" "$b" "${cmdnames[$b]}" "record" "${args[$b]}" # insert
-        newstage "3"
-        runmode "$P" "$b" "${cmdnames[$b]}" "record" "${args[$b]}" # check conflicts
+        newstage "3" "2"
+        runmode "$P" "$b" "${cmdnames[$b]}" "record" "${args[$b]}" # conflict
+
+        printf "\n"
+        printf "%-10s" "$b"
+        printf "%5s" $P
+        printf "%10s" "pre"
+        newstage "0" "1"
+        runmode "$P" "$b" "${cmdnames[$b]}" "none" "${args[$b]}" # baseline
+        newstage "1" "1"
+        runmode "$P" "$b" "${cmdnames[$b]}" "record" "${args[$b]}" # get pedigree
+        newstage "2" "1"
+        runmode "$P" "$b" "${cmdnames[$b]}" "record" "${args[$b]}" # insert
+        newstage "3" "1"
+        runmode "$P" "$b" "${cmdnames[$b]}" "record" "${args[$b]}" # conflict
+
+
+        # newstage "3" "1"
+        # runmode "$P" "$b" "${cmdnames[$b]}" "none" "${args[$b]}"
+        # newstage "3" "1"
+        # runmode "$P" "$b" "${cmdnames[$b]}" "record" "${args[$b]}"
+        
+        # newstage "3" "2"
+        # runmode "$P" "$b" "${cmdnames[$b]}" "none" "${args[$b]}"
+        # newstage "3" "2"
+        # runmode "$P" "$b" "${cmdnames[$b]}" "record" "${args[$b]}"
+        
         printf "\n"
     done
 done

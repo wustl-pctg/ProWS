a816471d8e042913a6e1af223235405dc3a2c072
robertutterback <robertu13@gmail.com>
2016-09-16 13:48:38 -0500

    minor changes

--- src/mutex.h
+++ src/mutex.h
@@ -9,41 +9,48 @@ extern "C" {
 
 namespace cilkrr {
 
-    class mutex {
-        private:
+  class mutex {
+  private:
 #ifdef USE_LOCKSTAT
-            struct spinlock_stat m_lock;
+    struct spinlock_stat m_lock;
 #else
-            pthread_spinlock_t m_lock;
+    pthread_spinlock_t m_lock;
 #endif
 
 #ifdef DEBUG_ACQUIRE
-            __cilkrts_worker *m_owner = nullptr; // just for debugging
-            acquire_info volatile *m_active = nullptr; // just for debugging??
+    __cilkrts_worker *m_owner = nullptr;
+    acquire_info volatile *m_active = nullptr;
+#endif
+#ifdef PORR_STATS
+    uint64_t m_num_acquires;
+    //uint64_t m_id;
 #endif
 
-            // Record/Replay fields
-            uint64_t m_id; /// index into global container of mutexes
-            acquire_container* m_acquires = nullptr; /// container of recorded acquires
-            volatile bool m_checking = false; /// For handoff between acquires
-            uint64_t m_num_acquires;
+    // Record/Replay fields
+    volatile bool m_checking = false; /// For handoff between acquires
+#ifdef ACQ_PTR
+    acquire_container* m_acquires;
+#else
+    acquire_container m_acquires;
+#endif
             
-            void record_acquire(pedigree_t& p);
-            void replay_lock(acquire_info *a);
-            void replay_unlock();
+    void record_acquire(pedigree_t& p);
+    void replay_lock(acquire_info *a);
+    void replay_unlock();
 
-            inline void acquire();
-            inline void release();
+    inline void acquire();
+    inline void release();
+    inline void init();
 
-        public:
-            mutex();
-            mutex(uint64_t index);
-            ~mutex();
+  public:
+    mutex();
+    mutex(uint64_t index);
+    ~mutex();
 
-            void lock();
-            void unlock();
-            bool try_lock();
+    void lock();
+    void unlock();
+    bool try_lock();
 
-    };
+  } __attribute__((aligned(64)));
 
 }

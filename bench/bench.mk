b479656bc6fc53b9e05603bef9e227a6a79fe018
robertutterback <robertu13@gmail.com>
2016-09-28 10:43:19 -0500

    Refactor common Makefile definitions for benchmarks

--- bench/bench.mk
+++ bench/bench.mk
@@ -11,18 +11,27 @@ CXX = $(COMPILER_DIR)/bin/clang++
 
 ifeq ($(PORR), 1)
     PORR_CFLAGS = -DPORR -fcilk-no-inline
-    PORR_LIBS = $(PORR_INSTALL)/libporr.a
+    PORR_LIBS = $(PORR_INSTALL)/build/libporr.a
 else 
     PORR_CFLAGS = -fcilk-no-inline
 endif
 
-LTO = -flto
-OPT = -O3 -march=native -DNDEBUG $(LTO)
-
 INC += -I$(COMPILER_DIR)/include -I$(CILKRTS_INSTALL)/include
-INC += -I$(BENCH_DIR) -I$(PORR_INSTALL)
-LIBS += -ldl -lpthread $(PORR_LIBS) $(CILKRTS_INSTALL)/lib/libcilkrts.a $(LTO) #-lpapi
-# LDFLAGS += -L$(CILKRTS_INSTALL)/lib
+INC += -I$(BENCH_DIR) -I$(PORR_INSTALL)/src
+LDFLAGS = 
+ARFLAGS = rcs
+OPT = -O3 -march=native -DNDEBUG
+
+
+LTO ?= 1
+ifeq ($(LTO),1)
+  OPT += -flto
+	LDFLAGS += -flto
+	ARFLAGS += --plugin $(COMPILER_HOME)/lib/LLVMgold.so
+endif
+
+
+LIBS += -ldl -lpthread -ltcmalloc $(PORR_LIBS) $(CILKRTS_INSTALL)/lib/libcilkrts.a
 BASIC_FLAGS = $(OPT) -g -fcilkplus $(PORR_CFLAGS) $(INC) #-Wfatal-errors
 BASIC_CFLAGS += $(BASIC_FLAGS) -std=gnu11
 BASIC_CXXFLAGS = $(BASIC_FLAGS) -std=gnu++11

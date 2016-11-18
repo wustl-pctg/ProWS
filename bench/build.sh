374d5e1b90324745290ee98990b0bac0ee7c2734
robertutterback <robertu13@gmail.com>
2016-11-18 15:42:38 -0600

    Adding bench script

---
+++ bench/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# Build all cilkplus-tests benchmarks
+./cilkplus-tests/build.sh
+
+cd ferret
+make clean
+make base -j
+make clean
+make -j
+
+cd -
+cd dedup
+make clean
+make base -j
+make clean
+make -j

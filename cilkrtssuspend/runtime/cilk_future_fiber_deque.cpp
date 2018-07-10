c68ac5a47e45c786a6a160d42fa96de2997890ad
Kyle Singer <southpaw63119@gmail.com>
2018-07-10 16:03:42 -0500

    Added ifdefs to switch between standard fib and testing combined parallelism in a single handcomp future function

---
+++ cilkrtssuspend/runtime/cilk_future_fiber_deque.cpp
@@ -0,0 +1,62 @@
+#include "cilk_fiber.h"
+#include "deque.h"
+#include "local_state.h"
+#include "scheduler.h"
+
+extern "C" {
+void __cilkrts_enqueue_future_fiber(cilk_fiber *fiber) {
+    __cilkrts_worker *curr_worker = __cilkrts_get_tls_worker_fast();
+
+    cilk_fiber *volatile *tail = curr_worker->l->active_deque->fiber_tail;
+    *tail++ = fiber;
+
+    curr_worker->l->active_deque->fiber_tail = tail;
+}
+
+#define BEGIN_WITH_WORKER_LOCK(w) __cilkrts_worker_lock(w); do
+#define END_WITH_WORKER_LOCK(w)   while (__cilkrts_worker_unlock(w), 0)
+
+cilk_fiber* __cilkrts_pop_tail_future_fiber() {
+    __cilkrts_worker *curr_worker = __cilkrts_get_tls_worker_fast();
+
+    cilk_fiber *volatile *t = curr_worker->l->active_deque->fiber_tail;
+    t--; 
+    curr_worker->l->active_deque->fiber_tail = t;
+    
+    
+    // TODO: I don't think we need to check if it is safe; it should
+    //       ALWAYS be safe, since we synchronize on the corresponding
+    //       stack frame when necessary.
+    //if (/*__builtin_expect(*/t < curr_worker->l->active_deque->fiber_head/*, 0)*/) {
+    //    int stolen_p = 0;
+
+    //    BEGIN_WITH_WORKER_LOCK(curr_worker) {
+    //        stolen_p = !(curr_worker->l->active_deque->fiber_head < (curr_worker->l->active_deque->fiber_tail + 1));
+
+    //        if (/*__builtin_expect(*/stolen_p/*, 0)*/) {
+    //            *curr_worker->l->active_deque->fiber_tail++;
+    //            CILK_ASSERT(curr_worker->l->active_deque->fiber_tail == curr_worker->l->active_deque->fiber_head);
+    //            CILK_ASSERT(!"There should be a fiber tail if we are popping");
+    //            return NULL;
+    //        }
+    //    } END_WITH_WORKER_LOCK(curr_worker);
+    //}
+
+    return *t;
+}
+
+cilk_fiber* __cilkrts_pop_head_future_fiber(__cilkrts_worker *victim, deque *d) {
+    // TODO: I don't think we need to check if it is safe; it should
+    //       ALWAYS be safe, since we synchronize on the corresponding
+    //       stack frame when necessary.
+    //if (/*__builtin_expect(*/fiber_dekker_protocol(victim, d)/*, 1)*/) {
+        cilk_fiber **h = (cilk_fiber **)d->fiber_head;
+        d->fiber_head = d->fiber_head + 1;
+        return *h;
+    //} else {
+    //    CILK_ASSERT("There should be a head when we pop future fibers");
+    //    return NULL;
+    //}
+}
+
+}
--- future-bench/handcomp_fib_cilkfut.cpp
+++ future-bench/handcomp_fib_cilkfut.cpp
@@ -15,7 +15,8 @@
 #endif
 
 //#define TEST_INTEROP_PRE_FUTURE_CREATE
-//#define TEST_INTEROP_POST_FUTURE_CREATE
+#define TEST_INTEROP_POST_FUTURE_CREATE
+//#define TEST_INTEROP_MULTI_FUTURE
 
 /* 
  * fib 39: 63245986
@@ -66,7 +67,14 @@ int  __attribute__((noinline)) fib(int n) {
     __cilkrts_stack_frame* sf = (__cilkrts_stack_frame*) alloca(sizeof(__cilkrts_stack_frame));;
     __cilkrts_enter_frame_1(sf);
 
+    volatile int done = 0;
+    volatile int done1 = 0;
+    volatile int done2 = 0;
+    volatile int done3 = 0;
+    volatile int done4 = 0;
+
     #ifdef TEST_INTEROP_PRE_FUTURE_CREATE
+        #pragma message ("using spawn pre fut fib interop")
         if (!CILK_SETJMP(sf->ctx)) {
             fib_helper(&y, n-2);
         }
@@ -96,15 +104,48 @@ int  __attribute__((noinline)) fib(int n) {
         __cilkrts_switch_fibers_back(sf, fut_fiber, initial_fiber);
     }
 
-    if (sf->flags & CILK_FRAME_STOLEN) {
-        CILK_ASSERT((sf->flags & CILK_FRAME_FUTURE_PARENT)==0);
-    }
-
     #ifdef TEST_INTEROP_POST_FUTURE_CREATE
+        #pragma messgae ("using using spawn post fut fib interop")
         if (!CILK_SETJMP(sf->ctx)) {
             fib_helper(&y, n-2);
         }
+    #elif defined(TEST_INTEROP_MULTI_FUTURE)
+        #pragma message ("using future fib interop")
+        cilk::future<int> y_fut = cilk::future<int>();
+
+        CILK_ASSERT((sf->flags & CILK_FRAME_FUTURE_PARENT) == 0);
+        sf->flags |= CILK_FRAME_FUTURE_PARENT;
+
+        cilk_fiber* initial_fiber2 = cilk_fiber_get_current_fiber();
+
+        __CILK_JUMP_BUFFER bkup2;
+     
+        if (!CILK_SETJMP(sf->ctx)) {
+            CILK_ASSERT(done == 0);
+            done = 1;
+            memcpy((void*)bkup2, sf->ctx, 5*sizeof(void*));
+            __cilkrts_switch_fibers(sf);
+    
+        } else if (sf->flags & CILK_FRAME_FUTURE_PARENT) {
+            CILK_ASSERT(done1 == 0);
+            done1 = 1;
+            memcpy(sf->ctx, (void*)bkup2, 5*sizeof(void*));
+    
+            fib_fut(&y_fut, n-1);
+    
+            cilk_fiber *fut_fiber = __cilkrts_pop_tail_future_fiber();
+            sf->flags &= ~(CILK_FRAME_FUTURE_PARENT);
+    
+            __cilkrts_switch_fibers_back(sf, fut_fiber, initial_fiber2);
+        }
+            CILK_ASSERT(done2 == 0);
+            done2 = 1;
+    
+            y = y_fut.get();
+            CILK_ASSERT(done3 == 0);
+            done3 = 1;
     #elif !defined(TEST_INTEROP_PRE_FUTURE_CREATE)
+        #pragma message ("using regular fib (no interop)")
         y = fib(n-2);
     #endif
 

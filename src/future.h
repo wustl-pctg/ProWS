8c9154cc70b48b3116c90a8a1b82502c19bdb997
Kyle Singer <kdsinger@wustl.edu>
2018-08-16 22:15:40 -0500

    Removed magic from future.h

--- src/future.h
+++ src/future.h
@@ -71,7 +71,6 @@ template<typename T>
 class future {
 private:
   volatile T m_result;
-  int magic;
 
   __cilkrts_deque_link head = {
     .d = NULL, .next = NULL
@@ -91,13 +90,11 @@ private:
 public:
 
  future() {
-    magic = 0xc1407a5;
     m_num_suspended_deques = 0;
     tail = &head;
   };
 
   ~future() {
-    magic = ~0xc1407a5;
   }
 
   inline void reset() {
@@ -138,8 +135,6 @@ public:
   } 
 
   T __attribute__((always_inline)) get() {
-    assert(this);
-    assert(magic == 0xc1407a5);
     if (!this->ready()) {
       suspend_deque();
     }

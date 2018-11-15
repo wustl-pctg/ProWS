650347a11982ed350fa6331565bdb63cab0fe793
Kyle Singer <kdsinger@wustl.edu>
2018-11-15 13:24:24 -0600

    Changed the method for mounting a folder in docker (to be backwards compatible on docker)

--- start_docker.sh
+++ start_docker.sh
@@ -7,4 +7,6 @@ if ! cmp config.mk docker_config.mk >/dev/null 2>&1 ; then
   cp docker_config.mk config.mk
 fi
 
-docker run -u=$UID:$(id -g $USER) -it --security-opt seccomp=unconfined --rm --mount src=/"$(pwd)",target=/mnt/cilk-plus-futures,type=bind -w=/mnt/cilk-plus-futures  -e LD_LIBRARY_PATH=/mnt/cilk-plus-futures/SuperMalloc/release/lib cilkfut
+#docker run -u=$UID:$(id -g $USER) -it --security-opt seccomp=unconfined --rm --mount src=/"$(pwd)",target=/mnt/cilk-plus-futures,type=bind -w=/mnt/cilk-plus-futures  -e LD_LIBRARY_PATH=/mnt/cilk-plus-futures/SuperMalloc/release/lib cilkfut
+
+docker run -u=$UID:$(id -g $USER) -it --security-opt seccomp=unconfined --rm -v$(pwd):/mnt/cilk-plus-futures -w=/mnt/cilk-plus-futures  -e LD_LIBRARY_PATH=/mnt/cilk-plus-futures/SuperMalloc/release/lib cilkfut

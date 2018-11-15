f6531a80cc0f80b27d2bfc1edcd2307f9360002d
Kyle Singer <southpaw63119@gmail.com>
2018-11-15 13:16:18 -0600

    Made some makefile fixes and changed some collection scripts to reflect new naming scheme

--- futurerd-bench/basic/collect_lcs_data.sh
+++ futurerd-bench/basic/collect_lcs_data.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-echo "16" | tee -a lcs-structured_result.txt lcs-unstructured_result.txt lcs-cilk-for_result.txt
-taskset -c 0-15 ./lcs-structured -n 32768 -b 512 | tee -a lcs-structured_result.txt
-taskset -c 0-15 ./lcs-unstructured -n 32768 -b 512 | tee -a lcs-unstructured_result.txt
-taskset -c 0-15 ./lcs-cilk-for -n 32768 -b 512 | tee -a lcs-cilk-for_result.txt
-echo "12" | tee -a lcs-structured_result.txt lcs-unstructured_result.txt lcs-cilk-for_result.txt
-taskset -c 0-11 ./lcs-structured -n 32768 -b 512 | tee -a lcs-structured_result.txt
-taskset -c 0-11 ./lcs-unstructured -n 32768 -b 512 | tee -a lcs-unstructured_result.txt
-taskset -c 0-11 ./lcs-cilk-for -n 32768 -b 512 | tee -a lcs-cilk-for_result.txt
-echo "8" | tee -a lcs-structured_result.txt lcs-unstructured_result.txt lcs-cilk-for_result.txt
-taskset -c 0-7 ./lcs-structured -n 32768 -b 512 | tee -a lcs-structured_result.txt
-taskset -c 0-7 ./lcs-unstructured -n 32768 -b 512 | tee -a lcs-unstructured_result.txt
-taskset -c 0-7 ./lcs-cilk-for -n 32768 -b 512 | tee -a lcs-cilk-for_result.txt
-echo "4" | tee -a lcs-structured_result.txt lcs-unstructured_result.txt lcs-cilk-for_result.txt
-taskset -c 0-3 ./lcs-structured -n 32768 -b 512 | tee -a lcs-structured_result.txt
-taskset -c 0-3 ./lcs-unstructured -n 32768 -b 512 | tee -a lcs-unstructured_result.txt
-taskset -c 0-3 ./lcs-cilk-for -n 32768 -b 512 | tee -a lcs-cilk-for_result.txt
-echo "2" | tee -a lcs-structured_result.txt lcs-unstructured_result.txt lcs-cilk-for_result.txt
-taskset -c 0-1 ./lcs-structured -n 32768 -b 512 | tee -a lcs-structured_result.txt
-taskset -c 0-1 ./lcs-unstructured -n 32768 -b 512 | tee -a lcs-unstructured_result.txt
-taskset -c 0-1 ./lcs-cilk-for -n 32768 -b 512 | tee -a lcs-cilk-for_result.txt
-echo "1" | tee -a lcs-structured_result.txt lcs-unstructured_result.txt lcs-cilk-for_result.txt
-taskset -c 0 ./lcs-structured -n 32768 -b 512 | tee -a lcs-structured_result.txt
-taskset -c 0 ./lcs-unstructured -n 32768 -b 512 | tee -a lcs-unstructured_result.txt
-taskset -c 0 ./lcs-cilk-for -n 32768 -b 512 | tee -a lcs-cilk-for_result.txt
+echo "16" | tee -a lcs-sf_result.txt lcs-gf_result.txt lcs-fj_result.txt
+taskset -c 0-15 ./lcs-sf -n 32768 -b 512 | tee -a lcs-sf_result.txt
+taskset -c 0-15 ./lcs-gf -n 32768 -b 512 | tee -a lcs-gf_result.txt
+taskset -c 0-15 ./lcs-fj -n 32768 -b 512 | tee -a lcs-fj_result.txt
+echo "12" | tee -a lcs-sf_result.txt lcs-gf_result.txt lcs-fj_result.txt
+taskset -c 0-11 ./lcs-sf -n 32768 -b 512 | tee -a lcs-sf_result.txt
+taskset -c 0-11 ./lcs-gf -n 32768 -b 512 | tee -a lcs-gf_result.txt
+taskset -c 0-11 ./lcs-fj -n 32768 -b 512 | tee -a lcs-fj_result.txt
+echo "8" | tee -a lcs-sf_result.txt lcs-gf_result.txt lcs-fj_result.txt
+taskset -c 0-7 ./lcs-sf -n 32768 -b 512 | tee -a lcs-sf_result.txt
+taskset -c 0-7 ./lcs-gf -n 32768 -b 512 | tee -a lcs-gf_result.txt
+taskset -c 0-7 ./lcs-fj -n 32768 -b 512 | tee -a lcs-fj_result.txt
+echo "4" | tee -a lcs-sf_result.txt lcs-gf_result.txt lcs-fj_result.txt
+taskset -c 0-3 ./lcs-sf -n 32768 -b 512 | tee -a lcs-sf_result.txt
+taskset -c 0-3 ./lcs-gf -n 32768 -b 512 | tee -a lcs-gf_result.txt
+taskset -c 0-3 ./lcs-fj -n 32768 -b 512 | tee -a lcs-fj_result.txt
+echo "2" | tee -a lcs-sf_result.txt lcs-gf_result.txt lcs-fj_result.txt
+taskset -c 0-1 ./lcs-sf -n 32768 -b 512 | tee -a lcs-sf_result.txt
+taskset -c 0-1 ./lcs-gf -n 32768 -b 512 | tee -a lcs-gf_result.txt
+taskset -c 0-1 ./lcs-fj -n 32768 -b 512 | tee -a lcs-fj_result.txt
+echo "1" | tee -a lcs-sf_result.txt lcs-gf_result.txt lcs-fj_result.txt
+taskset -c 0 ./lcs-sf -n 32768 -b 512 | tee -a lcs-sf_result.txt
+taskset -c 0 ./lcs-gf -n 32768 -b 512 | tee -a lcs-gf_result.txt
+taskset -c 0 ./lcs-fj -n 32768 -b 512 | tee -a lcs-fj_result.txt
--- futurerd-bench/basic/collect_sw_data.sh
+++ futurerd-bench/basic/collect_sw_data.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 
-echo "16" | tee sw-structured_result.txt sw-unstructured_result.txt sw-cilk-for_result.txt
-taskset -c 0-15 ./sw-structured -n 2048 | tee -a sw-structured_result.txt
-taskset -c 0-15 ./sw-unstructured -n 2048 | tee -a sw-unstructured_result.txt
-taskset -c 0-15 ./sw-cilk-for -n 2048 | tee -a sw-cilk-for_result.txt
-echo "12" | tee -a sw-structured_result.txt sw-unstructured_result.txt sw-cilk-for_result.txt
-taskset -c 0-11 ./sw-structured -n 2048 | tee -a sw-structured_result.txt
-taskset -c 0-11 ./sw-unstructured -n 2048 | tee -a sw-unstructured_result.txt
-taskset -c 0-11 ./sw-cilk-for -n 2048 | tee -a sw-cilk-for_result.txt
-echo "8" | tee -a sw-structured_result.txt sw-unstructured_result.txt sw-cilk-for_result.txt
-taskset -c 0-7 ./sw-structured -n 2048 | tee -a sw-structured_result.txt
-taskset -c 0-7 ./sw-unstructured -n 2048 | tee -a sw-unstructured_result.txt
-taskset -c 0-7 ./sw-cilk-for -n 2048 | tee -a sw-cilk-for_result.txt
-echo "4" | tee -a sw-structured_result.txt sw-unstructured_result.txt sw-cilk-for_result.txt
-taskset -c 0-3 ./sw-structured -n 2048 | tee -a sw-structured_result.txt
-taskset -c 0-3 ./sw-unstructured -n 2048 | tee -a sw-unstructured_result.txt
-taskset -c 0-3 ./sw-cilk-for -n 2048 | tee -a sw-cilk-for_result.txt
-echo "2" | tee -a sw-structured_result.txt sw-unstructured_result.txt sw-cilk-for_result.txt
-taskset -c 0-1 ./sw-structured -n 2048 | tee -a sw-structured_result.txt
-taskset -c 0-1 ./sw-unstructured -n 2048 | tee -a sw-unstructured_result.txt
-taskset -c 0-1 ./sw-cilk-for -n 2048 | tee -a sw-cilk-for_result.txt
-echo "1" | tee -a sw-structured_result.txt sw-unstructured_result.txt sw-cilk-for_result.txt
-taskset -c 0 ./sw-structured -n 2048 | tee -a sw-structured_result.txt
-taskset -c 0 ./sw-unstructured -n 2048 | tee -a sw-unstructured_result.txt
-taskset -c 0 ./sw-cilk-for -n 2048 | tee -a sw-cilk-for_result.txt
+INPUT_SIZE=4096
+BASE_CASE=64
+
+echo "16" | tee sw-sf_result.txt sw-gf_result.txt sw-fj_result.txt
+taskset -c 0-15 ./sw-sf -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-sf_result.txt
+taskset -c 0-15 ./sw-gf -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-gf_result.txt
+taskset -c 0-15 ./sw-fj -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-fj_result.txt
+echo "12" | tee -a sw-sf_result.txt sw-gf_result.txt sw-fj_result.txt
+taskset -c 0-11 ./sw-sf -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-sf_result.txt
+taskset -c 0-11 ./sw-gf -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-gf_result.txt
+taskset -c 0-11 ./sw-fj -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-fj_result.txt
+echo "8" | tee -a sw-sf_result.txt sw-gf_result.txt sw-fj_result.txt
+taskset -c 0-7 ./sw-sf -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-sf_result.txt
+taskset -c 0-7 ./sw-gf -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-gf_result.txt
+taskset -c 0-7 ./sw-fj -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-fj_result.txt
+echo "4" | tee -a sw-sf_result.txt sw-gf_result.txt sw-fj_result.txt
+taskset -c 0-3 ./sw-sf -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-sf_result.txt
+taskset -c 0-3 ./sw-gf -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-gf_result.txt
+taskset -c 0-3 ./sw-fj -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-fj_result.txt
+echo "2" | tee -a sw-sf_result.txt sw-gf_result.txt sw-fj_result.txt
+taskset -c 0-1 ./sw-sf -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-sf_result.txt
+taskset -c 0-1 ./sw-gf -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-gf_result.txt
+taskset -c 0-1 ./sw-fj -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-fj_result.txt
+echo "1" | tee -a sw-sf_result.txt sw-gf_result.txt sw-fj_result.txt
+taskset -c 0 ./sw-sf -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-sf_result.txt
+taskset -c 0 ./sw-gf -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-gf_result.txt
+taskset -c 0 ./sw-fj -n $INPUT_SIZE -b $BASE_CASE | tee -a sw-fj_result.txt

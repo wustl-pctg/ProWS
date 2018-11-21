4ced11013ecb1ca055ec51a4b7948946aef71c95
Kyle Singer <southpaw63119@gmail.com>
2018-11-21 16:24:57 -0600

    Some updates to the run-benches.py script

--- run-benchmarks.py
+++ run-benchmarks.py
@@ -5,8 +5,31 @@ import argparse
 import subprocess
 import benchargs
 import math
+import calendar
+import time
 
 search_dirs = "ferret future-bench futurerd-bench"
+
+def write_grouped_data(groups):
+    output = ""
+    for g in groups:
+        output += groups[g] + '\n\n'
+
+    with open('all_results.csv', 'w') as f:
+        f.write(output)
+
+def get_nruns_str(bench, nruns):
+    if "hw" in bench:
+        return ""
+    elif "bst" in bench:
+        return ""
+    elif "ferret" in bench:
+        return ""
+    elif "fib" in bench:
+        return " " + str(nruns)
+
+    return " -nruns " + str(nruns)
+
 
 def main():
     parser = argparse.ArgumentParser(description='Run benchmarks and output their results to a file.')
@@ -23,6 +46,23 @@ def main():
         print bench_list
         exit(0)
 
+    bench_groups = dict()
+    for bench in bench_list:
+        # get the benchmark name
+        # (they are named in the format <bench>-<implementation>)
+        group_key = bench.split('-')[0] 
+        group_key += benchargs.bench_args[bench]['args'].format('<P>', '<P*4>')
+        # Create the group's header
+        group_val = benchargs.bench_args[bench]['args'].format('<P>', '<P*4>')
+        group_val += '\nrunning times (s):,'
+        for p in benchargs.core_counts:
+            group_val += 'P='+str(p)+' avg time (s),'+'stdev (s),'+'stdev (%),'
+        bench_groups[group_key] = group_val
+
+    #print bench_groups
+    #exit(0)
+
+    subprocess.call(['cp', '-r', 'bench-results', 'bkup-'+str(int(calendar.timegm(time.gmtime())))])
     subprocess.call(['mkdir', '-p', 'bench-results'])
 
     for bench in bench_list:
@@ -31,10 +71,13 @@ def main():
         (stdout, stderr) = location.communicate()
         location = stdout.decode().strip() + '/' + bench
 
+        group_key = bench.split('-')[0]
+        group_key += benchargs.bench_args[bench]['args'].format('<P>', '<P*4>')
+
         header = benchargs.bench_args[bench]['args'].format('<P>', '<P*4>') + '\nrunning times (s):,'
         data = bench + ','
         
-        for ncores in benchargs.core_counts:
+        for ncores in [16]:#benchargs.core_counts:
             if ('se' in bench):
                 ncores = 1
 
@@ -42,8 +85,13 @@ def main():
             avg = None
             stdev = None
 
-            for i in range(0, benchargs.bench_args[bench]['runs']):
-                cmd = "taskset -c 0-" + str(ncores-1) + " " + location + " " + benchargs.bench_args[bench]['args']
+            nruns = benchargs.bench_args[bench]['runs']
+            nruns_args_str = get_nruns_str(bench, nruns)
+            if nruns_args_str != "":
+                nruns = 1 # It is configurable in the benchmark
+
+            for i in range(0, nruns):
+                cmd = "taskset -c 0-" + str(ncores-1) + " " + location + " " + benchargs.bench_args[bench]['args'] + nruns_args_str
                 cmd = cmd.format(ncores, ncores*4)
                 print cmd
                 res = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -71,7 +119,7 @@ def main():
 
             if avg is None:
                 if (len(times) == 0):
-                    print "ERR running", bench, ". Was it configured to run less than once?"
+                    print "Skipping ", bench, " due to lack of timing data. Was it configured to run less than once?"
                     continue
                 avg = sum(times) / len(times)
                 if (len(times) > 1):
@@ -91,6 +139,8 @@ def main():
             if ('se' in bench):
                 break
 
+        bench_groups[group_key] = bench_groups[group_key] + '\n' + data
+        write_grouped_data(bench_groups)
         with open('bench-results/'+bench+'_timing.csv', 'w') as f:
             f.write(header)
             f.write('\n')
